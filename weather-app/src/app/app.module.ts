aaf82b7e0cafa26bd86c3d6deff03e95f883ea1c
Darshan Suthar <darshansuthar@live.in>
2018-02-25 20:08:13 +0530

    Add service and changes in HTML.

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,18 +1,18 @@
+Hi
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent
+ ],
+ imports: [
+   BrowserModule,
+   HttpClientModule
+ ],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
