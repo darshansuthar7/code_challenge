12107687518d2a2623daa6b3d85dd9550cd852f8
Darshan Suthar <darshansuthar@live.in>
2018-02-25 22:54:55 +0530

    Added component implementation for process Weather data.

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {WeatherDateDetail} from './WeatherDateDetail';
+import {WeatherService} from './WeatherService';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+    dates: string[] = [];
+    forecastDate: string;
+    dayDetail: WeatherDateDetail[] = [];
+    hourlyData: WeatherDateDetail[] = [];
+
+
+    constructor(private weatherService: WeatherService){}
+ 
+    ngOnInit(): void {
+        this.setWeatherData();
+    }
+ 
+    setWeatherData(): void {
+        this.weatherService.getWeatherData()
+        .subscribe(data => {
+        for(let elem of data.list) {
+        this.dayDetail.push(elem);
+        }
+        //console.log(this.dayDetail);
+        });
+    }
+ 
 }
+
