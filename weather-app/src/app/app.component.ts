cdb2f718d49575732d4f4884d452b2bc3880f765
Darshan Suthar <darshansuthar@live.in>
2018-02-25 23:28:44 +0530

    Updating working application with remained resources.

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -25,12 +25,34 @@ export class AppComponent {
     setWeatherData(): void {
         this.weatherService.getWeatherData()
         .subscribe(data => {
-        for(let elem of data.list) {
-        this.dayDetail.push(elem);
-        }
-        //console.log(this.dayDetail);
+            for(let elem of data.list) {
+                this.dayDetail.push(elem);
+            }
+            this.processData();
         });
     }
+    processData(): void {
+        for(let dtStr of this.dayDetail) {
+            const d_item: Date = new Date(Date.parse(dtStr.dt_txt));
+            this.dates.push(d_item.getDate() +"-" + (d_item.getMonth() + 1) +"-"+ d_item.getFullYear());	
+        }
+        this.dates = Array.from(new Set(this.dates))
+        this.dates = this.dates.slice(0,5);
+        this.showWeather(this.dates[0]);
+     }
+
+     showWeather(dateStr: string): void {
+        this.hourlyData = [];
+        this.forecastDate = dateStr;
+        for(let dtStr of this.dayDetail) { 
+            const d_item: Date = new Date(Date.parse(dtStr.dt_txt));
+            var hourDate = d_item.getDate() +"-" + (d_item.getMonth() + 1) +"-"+ d_item.getFullYear();
+            if(hourDate == dateStr) {
+                this.hourlyData.push(dtStr);
+            }
+        }
+     }
  
 }
+
 
